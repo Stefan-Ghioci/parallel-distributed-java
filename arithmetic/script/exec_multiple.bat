1476a47c2e96485c5c0835a71987aeabc3deaa19
Stefan Ghioci <ghioci.stefan@gmail.com>
2019-10-20 21:28:01 +0300

    Increase maximum memory pool to avoid Java heap size error

--- arithmetic/script/exec_multiple.bat
+++ arithmetic/script/exec_multiple.bat
@@ -1,2 +1,2 @@
 @echo off  
-for /L %%N in (1,1,%6) do "C:\Program Files\Java\jdk-11.0.2\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA 2018.3.5\lib\idea_rt.jar=51060:C:\Program Files\JetBrains\IntelliJ IDEA 2018.3.5\bin" -Dfile.encoding=UTF-8 -classpath %1 %2 %3 %4 %5 %%N
+for /L %%N in (1,1,%6) do "C:\Program Files\Java\jdk-11.0.2\bin\java.exe" -Xmx4096m "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA 2018.3.5\lib\idea_rt.jar=51060:C:\Program Files\JetBrains\IntelliJ IDEA 2018.3.5\bin" -Dfile.encoding=UTF-8 -classpath %1 %2 %3 %4 %5 %%N
